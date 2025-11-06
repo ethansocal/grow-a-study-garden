07b61c374f4d017af2bb7cbea5362639f3953cee
Ethan Henry <ehtanhnery@gmail.com>
2025-11-05 23:44:46 -0800

    style: remove extra function

--- scripts/global.gd
+++ scripts/global.gd
@@ -4,8 +4,3 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	DisplayServer.window_set_min_size(Vector2i(1024, 720))
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
