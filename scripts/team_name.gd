564761f3456913614079e8e0cf26e738b3d8bdcc
Ethan Henry <ehtanhnery@gmail.com>
2025-11-05 21:17:44 -0800

    Add aseprite and font auto size labels, prototype title screen

---
+++ scripts/team_name.gd
@@ -0,0 +1,12 @@
+extends Label
+
+var showingTeamName = true
+@onready var animationPlayer = $AnimationPlayer
+
+func _on_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		if showingTeamName:
+			animationPlayer.play("To Name")
+		else:
+			animationPlayer.play_backwards("To Name")
+		showingTeamName = !showingTeamName
