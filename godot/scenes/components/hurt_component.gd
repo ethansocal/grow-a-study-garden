6566675f1ea109c390a6e87e4023f51a473448dc
yvespansaintlaurent <ypan399@students.fairmontschools.com>
2026-03-10 21:11:45 -0700

    working corn

---
+++ godot/scenes/components/hurt_component.gd
@@ -0,0 +1,18 @@
+class_name HurtComponent
+extends Area2D
+
+enum Tools {
+	None,
+	Thing,
+	WaterCrops,
+	ChopWood,
+	PlantCorn,
+}
+
+@export var tool : Tools = Tools.None
+
+signal on_hurt
+
+
+func _on_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
