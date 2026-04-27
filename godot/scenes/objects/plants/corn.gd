8dd0f9e1dbcf36766923774b36177a2b04ce486c
yvespansaintlaurent <ypan399@students.fairmontschools.com>
2026-04-27 14:18:55 -0700

    update

--- godot/scenes/objects/plants/corn.gd
+++ godot/scenes/objects/plants/corn.gd
@@ -6,6 +6,8 @@ var corn_harvest_scene = preload("res://scenes/objects/plants/corn_harvest.tscn"
 @onready var watering_particles: GPUParticles2D = $WateringParticles
 @onready var flowering_particles: GPUParticles2D = $FloweringParticles
 @onready var hurt_component: HurtComponent = $HurtComponent
+@onready var growth_cycle_component: GrowthCycleComponent = $GrowthCycleComponent
+
 
 var growth_state: DataTypes.GrowthStates = DataTypes.GrowthStates.Seed
 
@@ -16,6 +18,18 @@ func _ready() -> void:
 	hurt_component.hurt.connect(on_hurt)
 	growth_cycle_component.crop_maturity.connect(on_crop_maturity)
 	growth_cycle_component.crop_harvesting.connect(on_crop_harvesting)
+	
+func _process(delta: float) -> void:
+	growth_state = growth_cycle_component.get_current_growth_state()
+	sprite_2d.frame = growth_state
+	
+	if growth_state == DataTypes.GrowthStates.Maturity:
+		flowering_particles.emitting = true
+		
+	
+	
+
+
 
 func on_hurt(hit_damage: int) -> void:
 	if !growth_cycle_component.is_watered:
@@ -26,4 +40,9 @@ func on_hurt(hit_damage: int) -> void:
 		
 func on_crop_maturity() -> void:
 	flowering_particles.emitting = true
+	
+func on_crop_harvesting() -> void:
+	var corn_harvest_instance = corn_harvest_scene.instantiate() as Node2D
+	corn_harvest_instance.global_position = global_position
+	get_parent().add_child(corn_harvest_instance)
 	
